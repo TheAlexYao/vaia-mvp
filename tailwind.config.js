25138bec5a20998505dcacc64c637a497b747c7c
Alexander Yao <9095664+TheAlexYao@users.noreply.github.com>
2025-01-19 16:54:31 +0700

    feat: Add brand color & gradients to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        vaiIndigo: '#1e3a8a',
+      },
+      backgroundImage: {
+        'dusk': 'linear-gradient(to right, #9333ea, #2563eb)',
+        'dawn': 'linear-gradient(to right, #60a5fa, #9333ea)',
+      },
+    },
   },
   plugins: [],
 }
